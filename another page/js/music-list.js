69ec87d0af67476b6fcf6a5e937da60ca2db3880
ELMEHDIKARIM <96936422+ELMEHDIKARIM@users.noreply.github.com>
2022-01-01 17:15:52 +0800

    Add files via upload

---
+++ another page/js/music-list.js
@@ -0,0 +1,44 @@
+<!-- this Code was made by El Mehdi Karim W19103330113 -->
+<!-- i hope you appreciate the effort and time i put into this project -->
+<!-- if you have any question you can contact me -->
+<!-- if you need any explanation contact me -->
+<!-- thank You :D -->
+let allMusic = [
+  {
+    name: "Rock With You",
+    artist: "Michael Jackson",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Hit Em Up",
+    artist: "2Pac",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Lucky You",
+    artist: "Eminem",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Life",
+    artist: "Alpha Blondy",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Over Now",
+    artist: "Post Malone",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "TAYER",
+    artist: "7liwa",
+    img: "music-6",
+    src: "music-6"
+  },
+  
+];
